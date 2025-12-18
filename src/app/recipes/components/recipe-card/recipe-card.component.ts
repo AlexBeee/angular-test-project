3db4e7b4fef2a4254f26c14a15d85b36b02c3119
Alexander Bernhardt <alexander.bernhardt@scitlab.de>
2025-12-18 12:44:52 +0100

    add directory and file structure

---
+++ src/app/recipes/components/recipe-card/recipe-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card.component',
+  imports: [],
+  templateUrl: './recipe-card.component.html',
+  styleUrl: './recipe-card.component.css',
+})
+export class RecipeCardComponent {
+
+}
---
+++ src/app/recipes/pages/recipe-form/recipe-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-form.component',
+  imports: [],
+  templateUrl: './recipe-form.component.html',
+  styleUrl: './recipe-form.component.css',
+})
+export class RecipeFormComponent {
+
+}
